c4057324b3455b39d5655b34c8844371d2a0d2db
Dakouri M. Kobri <maurille.kobri@gmail.com>
2023-12-25 14:16:23 +0100

    Set up server

---
+++ index.js
@@ -0,0 +1,9 @@
+// NPM Packages
+const express = require('express');
+
+const app = express();
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
