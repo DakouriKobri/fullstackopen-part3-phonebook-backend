fa3bc2b70e3f191713026e57bf05aa49b96ae519
Dakouri M. Kobri <maurille.kobri@gmail.com>
2023-12-26 12:41:53 +0100

    Complete exercise 3.2

--- index.js
+++ index.js
@@ -30,6 +30,48 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+  const numberOfPeople = persons.length;
+
+  const peopleInfo =
+    numberOfPeople === 0
+      ? 'Phonebook is empty.'
+      : `Phone book has info for ${numberOfPeople} ${
+          numberOfPeople > 1 ? 'people' : 'person'
+        }.`;
+
+  function dateTimeFormat(options) {
+    return new Intl.DateTimeFormat('en-US', options);
+  }
+
+  const weekday = dateTimeFormat({
+    weekday: 'short',
+  }).format();
+
+  const monthAndDay = dateTimeFormat({
+    month: 'short',
+    year: '2-digit',
+  }).format();
+
+  const time = dateTimeFormat({
+    timeStyle: 'long',
+    hour12: false,
+  }).format();
+
+  const timeZoneName = dateTimeFormat({ timeStyle: 'full' })
+    .formatToParts()
+    .find((item) => item.type === 'timeZoneName').value;
+
+  const dateTimeInfo = `${weekday} ${monthAndDay} ${time} (${timeZoneName})`;
+
+  const info = `
+  <p>${peopleInfo}</p>
+  <p>${dateTimeInfo}</p>
+  `;
+
+  response.send(info);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
