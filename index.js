d091f465d508a1ad21a5839452ad8416b6e3fb46
Dakouri M. Kobri <maurille.kobri@gmail.com>
2023-12-28 20:48:57 +0100

    Add environment variable to PORT for deployment & add cors

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ morgan.token('body', (request, response) => {
 });
 
 app.use(express.json());
+app.use(cors());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
@@ -129,7 +130,7 @@ app.post('/api/persons', (request, response) => {
   response.status(201).json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
