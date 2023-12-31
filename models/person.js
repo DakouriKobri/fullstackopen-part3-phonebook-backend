f276c3d21b0b03491190437c0ca2905894796f8d
Dakouri M. Kobri <maurille.kobri@gmail.com>
2023-12-31 17:44:04 +0100

    Correct typo

--- models/person.js
+++ models/person.js
@@ -20,7 +20,7 @@ const personSchema = mongoose.Schema({
 });
 
 personSchema.set('toJSON', {
-  Transform: (document, returnedObject) => {
+  transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
