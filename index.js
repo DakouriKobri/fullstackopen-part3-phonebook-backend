d3390e48f6ea52498bf3e06531b6b3fcf060920e
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-01-02 13:09:31 +0100

    Update info route handler

--- index.js
+++ index.js
@@ -9,28 +9,7 @@ const Person = require('./models/person');
 
 const app = express();
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
+let persons = [];
 
 function dateTimeFormat(options) {
   return new Intl.DateTimeFormat('en-US', options);
@@ -61,7 +40,13 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
+  Person.find({})
+    .then((fetchedPersons) => {
+      persons = fetchedPersons;
+    })
+    .catch((error) => next(error));
+
   const numberOfPeople = persons.length;
 
   const peopleInfo =
